2adb3fc6137a4158c0ae58ddb659245f85c614c2
Thitiphong Wancham <thitiphongwancham@Thitiphongs-MacBook-Air.local>
2025-04-01 07:21:02 +0700

    Move GameStore.API to src directory

--- Backend/src/GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
+++ Backend/src/GameStore.API/obj/Debug/net8.0/GameStore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b810c64c0dcdcc74a69ff6a78bd78826d8eb86b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
