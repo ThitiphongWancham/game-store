9c10345734ba2403e8310e7b5abd7106e4d95947
Thitiphong Wancham <thitiphongwancham@Thitiphongs-MacBook-Air.local>
2025-04-08 21:33:46 +0700

    Move in-memory data into GameStoreData class in Data directory

---
+++ Backend/src/GameStore.API/Data/GameStoreData.cs
@@ -0,0 +1,69 @@
+using GameStore.API.Models;
+
+namespace GameStore.API.Data;
+
+public class GameStoreData
+{
+    private readonly List<Genre> genres =
+    [
+        new Genre { Id = new Guid("7FF08C09-B1CF-472C-BEE5-376553D1B2D1"), Name = "Fighting" },
+        new Genre { Id = new Guid("1666AAF5-F67F-4A64-9000-A41E84F9E39D"), Name = "Kids and Family" },
+        new Genre { Id = new Guid("13BA7410-4051-43A2-B2A2-AB601EB19891"), Name = "Racing" },
+        new Genre { Id = new Guid("362FF8CE-44D5-40DA-9D1E-A794474B1654"), Name = "Roleplaying" },
+        new Genre { Id = new Guid("0EE921DE-FC06-4B9C-9E45-28E6905D4F77"), Name = "Sports" },
+    ];
+
+    private readonly List<Game> games;
+
+    public GameStoreData()
+    {
+        games = [
+            new Game
+            {
+                Id = Guid.NewGuid(),
+                Name = "Street Fighter II",
+                Genre = genres[0],
+                Price = 19.99m,
+                ReleaseDate = new DateOnly(1992, 7, 15),
+                Description = "Test description",
+            },
+            new Game
+            {
+                Id = Guid.NewGuid(),
+                Name = "Final Fantasy XIV",
+                Genre = genres[3],
+                Price = 59.99m,
+                ReleaseDate = new DateOnly(2010, 9, 30),
+                Description = "Test description",
+            },
+            new Game
+            {
+                Id = Guid.NewGuid(),
+                Name = "FIFA 23",
+                Genre = genres[4],
+                Price = 69.99m,
+                ReleaseDate = new DateOnly(2022, 9, 27),
+                Description = "Test description",
+            },
+        ];
+    }
+
+    public IEnumerable<Game> GetGames() => games;
+
+    public Game? GetGameById(Guid id) => games.Find(game => game.Id == id);
+
+    public void AddGame(Game newGame)
+    {
+        newGame.Id = Guid.NewGuid();
+        games.Add(newGame);
+    }
+
+    public void RemoveGame(Guid id)
+    {
+        games.RemoveAll(game => game.Id == id);
+    }
+
+    public IEnumerable<Genre> GetGenres() => genres;
+
+    public Genre? GetGenreById(Guid id) => genres.Find(genre => genre.Id == id);
+}
--- Backend/src/GameStore.API/Program.cs
+++ Backend/src/GameStore.API/Program.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using GameStore.API.Data;
 using GameStore.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,50 +7,12 @@ var app = builder.Build();
 
 const string GetGameEndpointName = "GetGameById";
 
-List<Genre> genres =
-[
-    new Genre { Id = new Guid("7FF08C09-B1CF-472C-BEE5-376553D1B2D1"), Name = "Fighting" },
-    new Genre { Id = new Guid("1666AAF5-F67F-4A64-9000-A41E84F9E39D"), Name = "Kids and Family" },
-    new Genre { Id = new Guid("13BA7410-4051-43A2-B2A2-AB601EB19891"), Name = "Racing" },
-    new Genre { Id = new Guid("362FF8CE-44D5-40DA-9D1E-A794474B1654"), Name = "Roleplaying" },
-    new Genre { Id = new Guid("0EE921DE-FC06-4B9C-9E45-28E6905D4F77"), Name = "Sports" },
-];
-
-List<Game> games =
-[
-    new Game
-    {
-        Id = Guid.NewGuid(),
-        Name = "Street Fighter II",
-        Genre = genres[0],
-        Price = 19.99m,
-        ReleaseDate = new DateOnly(1992, 7, 15),
-        Description = "Test description",
-    },
-    new Game
-    {
-        Id = Guid.NewGuid(),
-        Name = "Final Fantasy XIV",
-        Genre = genres[3],
-        Price = 59.99m,
-        ReleaseDate = new DateOnly(2010, 9, 30),
-        Description = "Test description",
-    },
-    new Game
-    {
-        Id = Guid.NewGuid(),
-        Name = "FIFA 23",
-        Genre = genres[4],
-        Price = 69.99m,
-        ReleaseDate = new DateOnly(2022, 9, 27),
-        Description = "Test description",
-    },
-];
+GameStoreData data = new();
 
 // ========================= GET =========================
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/games", () => games.Select(game => new GameSummaryDto(
+app.MapGet("/games", () => data.GetGames().Select(game => new GameSummaryDto(
     game.Id,
     game.Name,
     game.Genre.Name,
@@ -59,7 +22,7 @@ app.MapGet("/games", () => games.Select(game => new GameSummaryDto(
 
 app.MapGet("/games/{id}", (Guid id) =>
 {
-    Game? game = games.Find(game => game.Id == id);
+    Game? game = data.GetGameById(id);
     return game is null
         ? Results.NotFound()
         : Results.Ok
@@ -77,14 +40,13 @@ app.MapGet("/games/{id}", (Guid id) =>
 }).WithName(GetGameEndpointName);
 
 app.MapGet("/genres", () =>
-{
-    return Results.Ok(genres.Select(genre => new GenreDto(genre.Id, genre.Name)));
-});
+    data.GetGenres()
+        .Select(genre => new GenreDto(genre.Id, genre.Name)));
 
 // ========================= POST =========================
 app.MapPost("/games", (CreateGameDto game) =>
 {
-    var genre = genres.Find(genre => genre.Id == game.GenreId);
+    var genre = data.GetGenreById(game.GenreId);
     if (genre is null)
     {
         return Results.BadRequest("Invalid genre id");
@@ -92,7 +54,6 @@ app.MapPost("/games", (CreateGameDto game) =>
 
     var newGame = new Game
     {
-        Id = Guid.NewGuid(),
         Genre = genre,
         Name = game.Name,
         Price = game.Price,
@@ -100,7 +61,7 @@ app.MapPost("/games", (CreateGameDto game) =>
         Description = game.Description,
     };
 
-    games.Add(newGame);
+    data.AddGame(newGame);
 
     return Results.CreatedAtRoute
     (
@@ -121,14 +82,14 @@ app.MapPost("/games", (CreateGameDto game) =>
 // ========================= PUT =========================
 app.MapPut("/games/{id}", (Guid id, UpdateGameDto gameDto) =>
 {
-    var existingGame = games.Find(game => game.Id == id);
+    var existingGame = data.GetGameById(id);
 
     if (existingGame is null)
     {
         return Results.NotFound();
     }
 
-    var genre = genres.Find(genre => genre.Id == gameDto.GenreId);
+    var genre = data.GetGenreById(gameDto.GenreId);
 
     if (genre is null)
     {
@@ -147,7 +108,7 @@ app.MapPut("/games/{id}", (Guid id, UpdateGameDto gameDto) =>
 // ========================= DELETE =========================
 app.MapDelete("games/{id}", (Guid id) =>
 {
-    games.RemoveAll(game => game.Id == id);
+    data.RemoveGame(id);
     return Results.NoContent();
 });
 
